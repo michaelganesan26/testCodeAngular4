503ff27ca7dd5598824f576781536ceab18a4025
Michael Ganesan <cookieganesan@gmail.com>
2018-03-01 20:05:21 -0500

    Input with events (Sample Code)

--- src/app/test-code/event-driven/event-driven.component.ts
+++ src/app/test-code/event-driven/event-driven.component.ts
@@ -14,8 +14,12 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class EventDrivenComponent implements OnInit {
 
+  //variables
   @Output() dataMouseClick:EventEmitter<IBill> = new EventEmitter();
   bills:IBill[];
+
+  userName?:string="";
+
   constructor(bills: MockDataService,private logMessage:LoggerService) {
       this.bills = bills.getBills();
 
@@ -29,6 +33,18 @@ export class EventDrivenComponent implements OnInit {
      this.logMessage.logBill(item);
      //Send the message to the parent
      this.dataMouseClick.emit(item);
+
+  }
+  
+  trackUserName():void{
+     this.logMessage.log(this.userName);
+  }
+
+  onKeyUp(event:any) {
+
+      console.log(event.target.value);
+
+
 
   }
 
